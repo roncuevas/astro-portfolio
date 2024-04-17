21fb4934333a6be85e45d9bb7349c31d0ad7e9fc
Aarón Martínez Cuevas <github@roncuevas.com>
2024-04-16 18:24:27 -0600

    Replace with own data

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,88 +1,134 @@
 export const info = {
-  baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
-  jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  baseUrl: "https://www.roncuevas.com",
+  name: "Aaron A. Martinez Cuevas",
+  jobDescription: "iOS Developer",
+  about: `I'm an iOS developer based in the Mexico City Metropolitan Area. I am passionate about merging the worlds of medicine and technology, crafting applications that enhance research and healthcare. With a rich experience in bilingual technical support and as a freelance developer, I have developed robust skills in both iOS development and full-stack web development.
+
+  Over my career, I have accumulated expertise in Swift, SwiftUI, UIKit, and various backend technologies, successfully leading projects for platforms like Expedia and healthcare applications. Continuously seeking to expand my knowledge, I've also delved into Python, Firebase, and machine learning frameworks, which complement my extensive work with databases and server management on cloud platforms like GCP, Azure, and AWS.
   `,
 
-  experience: [
+  "experience": [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
-      endDate: "Present",
-      description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
-      ],
+      "name": "Luxoft",
+      "location": "Mexico City Metropolitan Area",
+      "startDate": "Jun 2023",
+      "endDate": "Apr 2024",
+      "description": [
+        "- Developed iOS applications, specifically for Expedia, focusing on enhancing user experience and feature integration.",
+        "- Collaborated extensively with cross-functional teams to design and deploy new application features."
+      ]
     },
-
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
-      description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
-      ],
+      "name": "NTT DATA Europe & Latam",
+      "location": "Mexico City Metropolitan Area",
+      "startDate": "Oct 2022",
+      "endDate": "Jun 2023",
+      "description": [
+        "- Involved in iOS development projects for clients including ENGIE, Chopo, Acceso Salud, and Actinver, focusing on UI/UX design and API integration.",
+        "- Performed code reviews and adhered to high standards in software quality to ensure efficient performance and reliability."
+      ]
     },
-
     {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
-      ],
+      "name": "Programa Delfin",
+      "location": "Mexico City Metropolitan Area",
+      "startDate": "Jun 2022",
+      "endDate": "Aug 2022",
+      "description": [
+        "- Research intern who participated in the analysis of polymorphisms in mitochondrial DNA associated with diabetes using bioinformatics tools."
+      ]
     },
+    {
+      "name": "Codifin",
+      "location": "Mexico City Metropolitan Area",
+      "startDate": "May 2022",
+      "endDate": "Aug 2022",
+      "description": [
+        "- Intern role focused on iOS development and full-stack web development, gaining practical experience in Swift and SwiftUI."
+      ]
+    },
+    {
+      "name": "LanguageLine Solutions",
+      "location": "Mexico City Metropolitan Area",
+      "startDate": "Apr 2022",
+      "endDate": "May 2022",
+      "description": [
+        "- Provided bilingual technical support as an over-the-phone interpreter, facilitating communication across diverse subject areas."
+      ]
+    },
+    {
+      "name": "SYKES",
+      "location": "Mexico City Metropolitan Area",
+      "startDate": "Oct 2021",
+      "endDate": "Jan 2022",
+      "description": [
+        "- Technical support agent level 2, delivering customer service and technical assistance for a range of electronic devices under an international insurance framework."
+      ]
+    },
+    {
+      "name": "Freelance",
+      "location": "Mexico City Metropolitan Area",
+      "startDate": "Aug 2018",
+      "endDate": "Jun 2021",
+      "description": [
+        "- Managed various freelance projects involving website creation, server management, and network setup across platforms including GCP, Azure, and AWS."
+      ]
+    }
   ],
 
-  education: [
+  "education": [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
-      description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
-      ],
+      "name": "Instituto Politécnico Nacional",
+      "location": "Mexico City, Mexico",
+      "startDate": "Aug 2018",
+      "endDate": "Aug 2025",
+      "description": [
+        "Bachelor of Medicine, Bachelor of Surgery - MBBS, Medicine. Currently in 9th semester, focusing on Medical Surgery."
+      ]
     },
     {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
+      "name": "Instituto Politécnico Nacional",
+      "location": "Mexico City, Mexico",
+      "startDate": "Oct 2021",
+      "endDate": "Dec 2025",
+      "description": [
+        "Bachelor's degree, Health Services Administration. Concentrating on Health Services Management."
+      ]
     },
     {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
+      "name": "Instituto Politécnico Nacional",
+      "location": "Mexico City, Mexico",
+      "startDate": "Feb 2016",
+      "endDate": "Jan 2018",
+      "description": [
+        "Technical degree, Computation. Covered web development, programming in Java, batch, C and C++, networks management, computer assembly, adobe suite, 3D modeling essentials, MySQL database management, operating systems, and Arduino projects."
+      ]
     },
+    {
+      "name": "UVEG",
+      "location": "Mexico",
+      "startDate": "Sep 2021",
+      "endDate": "Dec 2021",
+      "description": [
+        "Bachelor's degree, Management and business development. Focused on Administration and Business Development."
+      ]
+    },
+    {
+      "name": "UVEG",
+      "location": "Mexico",
+      "startDate": "Nov 2022",
+      "endDate": "Nov 2025",
+      "description": [
+        "Bachelor of Engineering, Computer Engineering. Engaged in comprehensive studies in computer engineering."
+      ]
+    }
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "mailto:uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    facebook: "https://www.facebook.com/Ron.Cuevas13/",
+    twitter: "https://twitter.com/RonCuevas13",
+    github: "https://github.com/roncuevas",
+    email: "mailto:contact@roncuevas.com",
+    linkedin: "https://www.linkedin.com/in/roncuevasx/",
   },
 
   projects: [
